d11060956446ad2b3db53821f4d5881ff1ca2052
amir <piramir77@gmail.com>
2021-12-26 05:24:47 +0330

    fix miss spell my name in user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Amir Pitmoradian',
+            'name' => 'Amir Pirmoradian',
             'email' => 'piramir77@gmail.com',
             'type' => 'super',
             'password' => Hash::make('yNy31q!WuN9PQ0Yh92ZN6Pt'),
